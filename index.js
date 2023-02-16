23bfa7847a7b83fd5c72a32f219db4e808108b1b
timotheegarot <timotheegarot@hotmail.com>
2023-02-16 19:27:39 +0100

    Accuracy error fixed

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const displayResult = () => {
   }
 
   document.getElementById("wpm").innerText = (userInput.value.length / 5 / timeTaken).toFixed(2) + "wpm";
-  document.getElementById("accuracy").innerText = Math.round(((userInput.value.length - mistakes) / userInput.length) * 100) + "%";
+  document.getElementById("accuracy").innerText = Math.round(((userInput.value.length - mistakes) / userInput.value.length) * 100) + "%";
 };
 
 const startTest = () => {
