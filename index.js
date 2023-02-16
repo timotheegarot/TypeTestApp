64134fd50387a1bca20b4e6b2a6f11436dc999b5
timotheegarot <timotheegarot@hotmail.com>
2023-02-16 18:44:06 +0100

    Quote display

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ let quote = "";
 let time = 60;
 let timer = "";
 let mistakes = 0;
+
+const renderNewQiote = async() => {
+  const response = await fetch(quoteApiUrl);
+  let data = await response.json();
+  quote = data.content;
+
+  let arr = quote.split("").map((value) => {
+    return "<span class='quote-chars>'" + value + "</span>";
+  })
+  quoteSection.innerHTML += arr.join("");
+}
